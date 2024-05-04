abd2e67351dfab3113580359c2ce4bdc0907b543
Bell <ds@inbox.ru>
2024-05-04 13:13:23 +0300

    commit

--- bot/handlers/user_commands.py
+++ bot/handlers/user_commands.py
@@ -2,12 +2,14 @@ from aiogram import Router, F, Bot
 from aiogram.types import Message, LabeledPrice, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.filters import Command, CommandObject, CommandStart
 from keyboards import reply, inline
+import requests
+import json
 
 router = Router()
 
 async def is_registered(tgid):
     is_tgids = str(tgid)
-    url = f"https://architecton.site/api/v1/account/tg/{tgids}"
+    url = f"https://architecton.site/api/v1/account/tg/{is_tgids}"
     response = requests.get(url)
     data = json.loads(response.text)
     if not data: 
@@ -16,7 +18,7 @@ async def is_registered(tgid):
 
 @router.message(CommandStart())
 async def start(msg: Message):
-    if await is_registered() == False:
+    if await is_registered(msg.from_user.id) == False:
         await msg.answer("Hi, you haven't registered yet. Connect your wallet here ⬇️⬇️⬇️", reply_markup=inline.not_register)
     else:
         await msg.answer("Hi, welcome back", reply_markup=inline.main)
