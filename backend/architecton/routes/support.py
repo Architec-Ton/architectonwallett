5ba8d38a5468e5a983ce4dcab564a434e6fd2055
Bell <ds@inbox.ru>
2024-05-09 02:33:06 +0300

    support page

--- backend/architecton/routes/support.py
+++ backend/architecton/routes/support.py
@@ -4,7 +4,7 @@ import logging
 import os.path
 from typing import List
 
-from fastapi import APIRouter, Form
+from fastapi import APIRouter, Form, Query
 from fastapi.templating import Jinja2Templates
 from tonsdk.utils import Address
 
@@ -13,7 +13,7 @@ from architecton.contracts.crowd_sale import CrowdSale
 from architecton.controllers.account_controller import AccountController
 from architecton.controllers.nftscan_controller import NFTscanController
 from architecton.controllers.ton_client import get_ton_client
-from architecton.models import Wallet, Notification, NotificationType, Account, Notcoin
+from architecton.models import Wallet, Notification, NotificationType, Account, Notcoin, ReferralsNotification
 from architecton.views.account import AccountBalanceOut, AccountIn, WalletOut
 from architecton.views.bank import BankUpdatesOut
 
@@ -34,31 +34,51 @@ render = Jinja2Templates(directory=f"{path}/templates")
 router = APIRouter(tags=["Support"])
 
 
+async def update_contect(context, request, address, username):
+    account = await Account.filter(username=username).first()
+
+    if account is None and address:
+        wallet = await Wallet.get_wallet(address)
+        if wallet is not None:
+            account = await Account.get_or_none(id=wallet.tg_id)
+
+    wo = []
+    notifications = []
+    refferer = []
+    if account is not None:
+        wallets = await Wallet.filter(tg_id=account.id)
+        notifications = await Notification.filter(tg_id=account.id).order_by("-created_at")
+        refferer = await ReferralsNotification.filter(tg_id=account.id).order_by("-created_at")
+        wo = []
+        for w in wallets:
+            banks = await AccountController.get_banks(w.address)
+
+            wo.append({"wallet": w, "banks": banks})
+
+    context.update({"account": account, "wallets": wo, "notifications": notifications, "refferer": refferer})
+
+    return context
+
+
 @router.get("")
-async def get_support_form(request: Request):
-    context = {"request": request}
+async def get_support_form(request: Request, address=Query(default=None), username=Query(default=None)):
+    context = {"request": request, "address": address, "username": username}
+
+    context = await update_contect(context, request, address, username)
+
+    logging.info(context)
     return render.TemplateResponse("support.html", context=context)
-
-
-@router.post("")
-async def post_support_form(request: Request, address: str = Form()):
-
-    banks = await AccountController.get_banks(address)
-
-    context = {"request": request, "banks": banks}
-
-    return render.TemplateResponse("output.html", context=context)
 
 
 @router.get("/test")
 async def get_test(request: Request):
-    # trxs = await AccountController.get_transactions("UQAeV4crAaUoCJo5igUIzosJXcOjtb4W7ff7Qr0DrgXPRle_")
-    #
-    # out = []
-    # for tx in trxs:
-    #     if tx.in_msg.source == "EQBhOhdA8vncTSH3ft2f-Nqj9PTmKTSZMbhkMN8DhFTeJC1g":
-    #         continue
-    #     print(tx.)
-    #     out.append(tx)
-    # return out
-    return await AccountController.update_notcoin()
+    trxs = await AccountController.get_transactions("UQAeV4crAaUoCJo5igUIzosJXcOjtb4W7ff7Qr0DrgXPRle_")
+
+    out = []
+    for tx in trxs:
+        if tx.in_msg.source == "EQBhOhdA8vncTSH3ft2f-Nqj9PTmKTSZMbhkMN8DhFTeJC1g":
+            continue
+        print(tx)
+        out.append(tx)
+    return out
+    # return await AccountController.update_notcoin()
