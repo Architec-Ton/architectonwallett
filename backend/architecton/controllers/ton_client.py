aae2f95808f3b8c0362ca0df1878df999332a693
Bell <ds@inbox.ru>
2024-05-21 16:53:00 +0300

    update on own server

--- backend/architecton/controllers/ton_client.py
+++ backend/architecton/controllers/ton_client.py
@@ -6,28 +6,30 @@ from TonTools import TonCenterClient, LsClient
 
 TON_TESTNET = int(os.getenv("TON_TESTNET", 0))
 
-TON_LSCLIENT = bool(os.getenv("TON_LSCLIENT", 1))
+TON_LSCLIENT = bool(os.getenv("TON_LSCLIENT", 0))
 
 
 client = TonCenterClient(
     testnet=bool(TON_TESTNET),
     key="88d5912ad2394e5cbae97a351bb6a3e1174e09f7956d096beaae3acab91324da",
+    base_url="https://ton.architecton.site/api/v2/",
 )
 
-lsclient = LsClient(ls_index=0, default_timeout=60, config="https://architecton.site/ton.config.json")
+# lsclient = LsClient(ls_index=0, default_timeout=60, config="https://architecton.site/ton.config.json")
 # lsclient = LsClient(ls_index=5, default_timeout=60)
 
 
 # jetton_wallet_data = await client.get_jetton_wallet(address)
 async def init_ton_client():
-    if bool(TON_LSCLIENT):
-        logging.info("Initialize ton ls client")
-        await asyncio.sleep(2)
-        await lsclient.init_tonlib()
-        logging.info("Done ton ls client")
+    pass
+    # if bool(TON_LSCLIENT):
+    #     logging.info("Initialize ton ls client")
+    #     await asyncio.sleep(2)
+    #     await lsclient.init_tonlib()
+    #     logging.info("Done ton ls client")
 
 
 def get_ton_client() -> TonCenterClient:
-    if bool(TON_LSCLIENT):
-        return lsclient
+    # if bool(TON_LSCLIENT):
+    #     return lsclient
     return client
