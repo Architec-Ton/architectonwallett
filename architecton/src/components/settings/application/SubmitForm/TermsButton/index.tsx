28c1b8cd4c54c4367057bf40f977af37f2bd5404
Bell <ds@inbox.ru>
2024-06-02 19:52:53 +0300

    add termas

--- architecton/src/components/settings/application/SubmitForm/TermsButton/index.tsx
+++ architecton/src/components/settings/application/SubmitForm/TermsButton/index.tsx
@@ -1,18 +1,21 @@
-import { t } from "i18next"
-import { Link } from "react-router-dom"
-import assets from "../../../../../assets"
+import { t } from 'i18next';
+import { Link } from 'react-router-dom';
+import assets from '../../../../../assets';
 
-import "./index.css"
+import './index.css';
+import { BASE_URL } from '../../../../../constants';
 
 const TermsButton = () => {
-    return (
-        <div className="input-container">
-            <Link className="terms-button" to="/settings/application-submit/terms">
-                <img src={assets.iconTerms} className="terms-button__icon" alt="" />
-                <span className="terms-button__text">{t("terms_label")}</span>
-            </Link>
-        </div>
-    )
-}
+  return (
+    <div className="input-container">
+      <Link
+        className="terms-button"
+        to={`${BASE_URL}/settings/application-submit/terms`}>
+        <img src={assets.iconTerms} className="terms-button__icon" alt="" />
+        <span className="terms-button__text">{t('terms_label')}</span>
+      </Link>
+    </div>
+  );
+};
 
-export default TermsButton
+export default TermsButton;
