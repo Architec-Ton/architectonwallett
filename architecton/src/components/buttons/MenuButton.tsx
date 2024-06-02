a06dc19114cb8085229b9e8b61f3781718281c26
Bell <ds@inbox.ru>
2024-06-02 19:41:53 +0300

    update

--- architecton/src/components/buttons/MenuButton.tsx
+++ architecton/src/components/buttons/MenuButton.tsx
@@ -1,14 +1,16 @@
+import { MouseEventHandler } from 'react';
 import './MenuButton.styles.css';
 
 type Props = {
   title: string;
   icon: string;
+  onClick?: MouseEventHandler;
 };
 
-function MenuButton({ title, icon }: Props) {
+function MenuButton({ title, icon, onClick }: Props) {
   return (
     <div className="menubutton">
-      <button>
+      <button onClick={onClick}>
         <img src={icon} alt={title} />
       </button>
       <div>{title}</div>
