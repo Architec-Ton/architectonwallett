c2ea4525d84de522cc850cabb6fd97ea73dd0860
Bell <ds@inbox.ru>
2024-07-15 03:19:59 +0300

    fix

--- bot/bot.py
+++ bot/bot.py
@@ -11,25 +11,22 @@ import handlers.user_commands
 from pooling import start_pooling
 
 bot_token = os.getenv("BOT_TOKEN", None)
-bot = Bot(bot_token, parse_mode='HTML')
-dp = Dispatcher() 
+bot = Bot(bot_token)
+dp = Dispatcher()
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.info("Бот запущен и работает...")
 
-async def main():  
-    
-    dp.include_routers(
-        handlers.user_commands.router,
-        handlers.bot_messages.router
-    )
+
+async def main():
+
+    dp.include_routers(handlers.user_commands.router, handlers.bot_messages.router)
     await bot.delete_webhook(drop_pending_updates=True)
     start_pooling(bot)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
-
